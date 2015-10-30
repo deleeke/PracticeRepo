78e5e1f4e44cd6ce10cd5a06fcfaa3303de8f863
Elijah <deleeke@gmail.com>
2015-10-30 10:21:54 -0400

    Added brute force method as outlined in text. Fixed error with the spaces.

--- cipher.py
+++ cipher.py
@@ -4,12 +4,12 @@ MAX_KEY_SIZE=26
 
 def getMode():
         while True:
-                print('Do you wish to encrypt or decrypt a message?')
+                print('Do you wish to encrypt, decrypt, or brute force decrypt a message?')
                 mode = input().lower()
-                if mode in 'encrypt e decrypt d'.split():
+                if mode in 'encrypt e decrypt d brute b'.split():
                         return mode
                 else:
-                        print('Enter either "encrypt" or "e" or "decrypt" or "d"')
+                        print('Enter either "encrypt" or "e" or "decrypt" or "d" or "brute" or "b"')
 
 def getMessage():
         print('Enter your message:')
@@ -25,31 +25,38 @@ def getKey():
 
 def getTranslatedMessage(mode,message,key):
         if mode[0]=='d':
-                key=-key
+                key = -key
         translated=''
 
         for symbol in message:
                 if symbol.isalpha():
                         num=ord(symbol)
                         num+=key
-                if symbol.isupper():
-                        if num>ord('Z'):
-                                num-=26
-                        elif num<ord('A'):
-                                num+=26
-                elif symbol.islower():
-                        if num>ord('z'):
-                                num-=26
-                        elif num<ord('a'):
-                                num+=26
+                        if symbol.isupper():
+                                if num>ord('Z'):
+                                        num-=26
+                                elif num<ord('A'):
+                                        num+=26
+                        elif symbol.islower():
+                                if num>ord('z'):
+                                        num-=26
+                                elif num<ord('a'):
+                                        num+=26
 
-                translated+=chr(num)
-        else:
-                translated+=symbol
+                        translated+=chr(num)
+                else:
+                        translated+=symbol
         return translated
+
 mode=getMode()
 message=getMessage()
-key=getKey()
+if mode[0]!='b':
+        key=getKey()
 
 print('Your translated text is:')
-print(getTranslatedMessage(mode, message, key))
+if mode[0]!='b':
+        print(getTranslatedMessage(mode, message, key))
+else:
+        for key in range(1, MAX_KEY_SIZE+1):
+                print(key, getTranslatedMessage('decrypt', message, key))
+                
