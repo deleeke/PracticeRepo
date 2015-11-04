7e3d5982dfee04f270a7edfcedacc91f420280cc
Elijah <deleeke@gmail.com>
2015-11-03 21:42:20 -0500

    Found bug -- needed to dedent region in isValidMove()

--- reversi.py
+++ reversi.py
@@ -31,7 +31,7 @@ def resetBoard(board):
     board[3][4] = 'O'
     board[4][3] = 'O'
     board[4][4] = 'X'
-
+    print('#def resetBoard() executed')
 
 
 def getNewBoard():
@@ -39,6 +39,7 @@ def getNewBoard():
     board = []
     for i in range(8):
         board.append([' '] * 8)
+    print('#def getNewBoard() executed')
     return board
 
 
@@ -47,14 +48,17 @@ def isValidMove(board, tile, xstart, ystart):
     # Returns False if the player's move on space xstart, ystart is invalid.
     # If it is a valid move, returns a list of spaces that would become the player's if they made a move here.
     if board[xstart][ystart] != ' ' or not isOnBoard(xstart,ystart):
+        print('isValidMove executed: moveis not valid')
         return False
 
     board[xstart][ystart] = tile #temporarily set the tile on the board.
 
     if tile == 'X':
         otherTile = 'O'
+        print('The computer\'s tile will be "O"')
     else:
         otherTile = 'X'
+        print('The computer\'s tile will be "X"')
 
     tilesToFlip = []
     for xdirection, ydirection in [[0,1], [1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1]]:
@@ -62,7 +66,7 @@ def isValidMove(board, tile, xstart, ystart):
         x += xdirection #first step in the direction
         y += ydirection #first step in the direction
         if isOnBoard(x, y) and board[x][y] == otherTile:
-            # There is a piece belonging to the other player next to our piece.
+            print('# There is a piece belonging to the other player next to our piece.')
             x += xdirection
             y += ydirection
             if not isOnBoard(x, y):
@@ -70,18 +74,19 @@ def isValidMove(board, tile, xstart, ystart):
             while board[x][y] == otherTile:
                 x += xdirection
                 y += ydirection
-                if not isOnBoard(x, y): #break out of while loop, then continue in for loop
-                    break
                 if not isOnBoard(x, y):
-                    continue
-                if board[x][y] == tile:
-                    # There are pieces to flip over. Go in the reverse direction until we reach the original space, noting all the tiles along the way.
-                    while True:
-                        x -= xdirection
-                        y -= ydirection
-                        if x == xstart and y == ystart:
-                            break
-                        tilesToFlip.append([x, y])
+                    print('#break out of while loop, then continue in for loop')
+                    break
+            if not isOnBoard(x, y):
+                continue
+            if board[x][y] == tile:
+                # There are pieces to flip over. Go in the reverse direction until we reach the original space, noting all the tiles along the way.
+                while True:
+                    x -= xdirection
+                    y -= ydirection
+                    if x == xstart and y == ystart:
+                        break
+                    tilesToFlip.append([x, y])
     board[xstart][ystart] = ' ' # restore the empty space
     if len(tilesToFlip) == 0: # If no tiles were flipped, this is not a valid move
         return False
